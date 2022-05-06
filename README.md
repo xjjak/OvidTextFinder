efc7fc2b1d4fe6f5f63d2991efb92fb35d47c48e
Zade <sirc3h4o2@gmail.com>
2022-05-06 09:48:36 +0200

    Added TODOS

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # OvidTextFinder
 
 Find the corresponding text in the works "Metamorphosis" by Ovid, given a list of latin words.
+
+### TODO: Scraper -> Get all texts from https://gottwein.de/Lat/ov/met01la.php
+
---
+++ analzyer.py
@@ -0,0 +1,8 @@
+
+def word_similarity():
+    # TODO: Compare principal form to word from text and evaluate similarity as value between 0-1(?)
+    pass
+
+def rank_text():
+    # TODO: Evaluate text rank based on word_similarity
+    pass
